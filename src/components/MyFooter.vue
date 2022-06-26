daef41aff25cdace5b6a78439ea1b437546ff91a
tongguan <tong_guan1024@163.com>
2022-06-26 19:40:35 +0800

    -购物车-总数量

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -14,10 +14,12 @@
     <!-- 合计 -->
     <div>
       <span>合计:</span>
-      <span class="price">¥ 0</span>
+      <span class="price">¥</span>
     </div>
     <!-- 按钮 -->
-    <button type="button" class="footer-btn btn btn-primary">结算 ( 0 )</button>
+    <button type="button" class="footer-btn btn btn-primary">
+      结算 ({{ allCount }})
+    </button>
   </div>
 </template>
 
@@ -35,6 +37,13 @@ export default {
       set(newValue) {
         this.list.forEach((item) => (item.goods_state = newValue));
       },
+    },
+    // -购物车-总数量
+    allCount() {
+      return this.list.reduce(
+        (pre, curr) => (curr.goods_state ? (pre += curr.goods_count) : pre),
+        0
+      );
     },
   },
 };
