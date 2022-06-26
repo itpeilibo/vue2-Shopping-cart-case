9434fe4556721cb0ebf407650a931e148940ab22
tongguan <tong_guan1024@163.com>
2022-06-26 19:33:51 +0800

    完成了商品选中、数量控制功能

---
+++ src/components/MyCount.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="my-counter">
+    <button
+      type="button"
+      class="btn btn-light"
+      @click="goods.goods_count--"
+      :disabled="goods.goods_count == 0"
+    >
+      -
+    </button>
+    <input type="number" class="form-control inp" v-model="goods.goods_count" />
+    <button type="button" class="btn btn-light" @click="goods.goods_count++">
+      +
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    goods: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.my-counter {
+  display: flex;
+  .inp {
+    width: 45px;
+    text-align: center;
+    margin: 0 10px;
+  }
+  .btn,
+  .inp {
+    transform: scale(0.9);
+  }
+}
+</style>
